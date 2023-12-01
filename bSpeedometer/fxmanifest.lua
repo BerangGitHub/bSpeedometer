c43f47e652a46fe33f96d67ab1558e3634d1d28e
Berang <berang0108@gmail.com>
2023-12-01 23:56:05 +0100

    main

--- bSpeedometer/fxmanifest.lua
+++ bSpeedometer/fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version "adamant"
 game "gta5"
 
+repository 'https://github.com/BerangGitHub/bSpeedometer'
+author 'Berang'
+description 'Speedometer by berang on Discord'
+
 client_script "client.lua"
 ui_page "html/index.html"
 
