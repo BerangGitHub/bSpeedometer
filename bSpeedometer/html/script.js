c83b69eb5c7b1a818644f2395f4b9b735ba599ec
Berang <berang0108@gmail.com>
2023-12-02 21:18:10 +0100

    Update script.js

--- bSpeedometer/html/script.js
+++ bSpeedometer/html/script.js
@@ -4,14 +4,14 @@ window.addEventListener("message", async (e) => {
       let speed = e.data.speed * 2.2369;
       $(".speed").text(speed.toFixed(0));
       if (speed < 10) {
-        $(".rec").animate({width: "145px"}, 13);
+        $(".rec").animate({width: "145px"}, 0);
       }
       else if (speed > 100) {
         console.log("test")
-        $(".rec").animate({width: "200px"}, 13);
+        $(".rec").animate({width: "200px"}, 0);
       }
       else if (speed >= 11) {
-        $(".rec").animate({width: "175px"}, 13);
+        $(".rec").animate({width: "175px"}, 0);
       }
   } else if (e.data.action == "cacher") {
     $("body").hide();
